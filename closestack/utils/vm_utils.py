d82dc6615a8c6c9be4ee4dfdb887b285360447c7
knktc <me@knktc.com>
2018-06-19 22:51:02 +0800

    finish functions to define vm

--- closestack/utils/vm_utils.py
+++ closestack/utils/vm_utils.py
@@ -9,7 +9,7 @@
 """
 
 import libvirt
-from .utils import cmd_runner
+from .utils import cmd_runner, format_vm_xml
 from xml.etree import ElementTree
 
 __author__ = 'knktc'
@@ -26,7 +26,7 @@ class VmManagerError(Exception):
 
 class VmManager(object):
     def __init__(self, conn_str, qemu_img_exec,
-                 qemu_kvm_exec):
+                 qemu_kvm_exec, template_path):
         """
         connect to host
         :param :
@@ -36,11 +36,17 @@ class VmManager(object):
         self.conn_str = conn_str
         self.qemu_img_exec = qemu_img_exec
         self.qemu_kvm_exec = qemu_kvm_exec
+        try:
+            with open(template_path, 'r') as f:
+                self.template = f.read()
+        except Exception as e:
+            raise VmManagerError("vm template file open failed")
 
         # try to connect to hypervisor
         self.conn = libvirt.open(self.conn_str)
         if self.conn is None:
             raise VmManagerError("connection failed, conn string: {}".format(self.conn_str))
+
 
     def close(self):
         """
@@ -90,4 +96,67 @@ class VmManager(object):
         else:
             return False
 
+    def define(self, vm_name, image_path, cpu_cores=1, memory=524288, host_passthrough=False,
+               persistent=False, boot=True):
+        """
+        define or create vm by xml
+
+        :param vm_name: vm name string
+        :param image_path: the vm will run with this image
+        :param cpu_cores: cpu cores
+        :param memory: ram size
+        :param host_passthrough: if true, we will set host passthrough to vm xml
+        :param persistent: persistent or not
+        :param boot: if true, the vm will boot after vm defined
+        :return: status code
+        :rtype: int
+
+        status
+        ========
+        0: success
+        1: xml format failed
+        2: create vm failed, unknown reason
+        3: vm successfully defined, but boot failed'
+
+        """
+        # config dict
+        config_dict = {
+            'vm_name': vm_name,
+            'image_path': image_path,
+            'qemu_kvm_exec': self.qemu_kvm_exec,
+            'memory': memory,
+            'current_memory': memory,
+            'cpu_cores': cpu_cores,
+        }
+
+        # add host passthrough config
+        if host_passthrough:
+            config_dict['host_passthrought'] = "<cpu mode='host-passthrough' check='none'/>"
+        else:
+            config_dict['host_passthrought'] = ''
+
+        # format xml
+        xml_config = format_vm_xml(template=self.template, config_dict=config_dict)
+        if xml_config:
+            return 1
+
+        # create vm
+        try:
+            if not persistent:
+                dom = self.conn.createXML(xml_config, 0)
+            else:
+                dom = self.conn.defineXML(xml_config)
+            if dom is None:
+                return 2
+        except Exception as e:
+            return 2
+
+        # try to boot vm
+        if persistent and boot:
+            if dom.create() < 0:
+                return 3
+            else:
+                return 0
+        else:
+            return 0
 
